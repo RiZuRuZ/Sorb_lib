ca35e074f4eae83674c5c812749fadf54499472e
RiZuRuZ <124576764+RiZuRuZ@users.noreply.github.com>
2024-02-19 20:52:38 +0700

    Update Sorb_lib.cpp

--- Sorb_lib.cpp
+++ Sorb_lib.cpp
@@ -1,4 +1,37 @@
 #include "Arduino.h"
+/*#define BLYNK_TEMPLATE_ID "TMPL6VSAGm9n1"
+#define BLYNK_TEMPLATE_NAME "TEST"
+#define BLYNK_AUTH_TOKEN "4lQZqwEu5QnRPcnviK2kY9DRsGFyHhkQ"
+#define BLYNK_FIRMWARE_VERSION "0.1.0"
+#define BLYNK_PRINT Serial
+#include <BlynkSimpleEsp32.h>*/
+
+/*
+//main
+//7ePoF1BYfYdNkzKMphtcB2Ncddi1IMEhZ0dX604zubi
+//D1h9vPDmynYSw9R7cvA9s2HMprLv9obFVHbZEhlw5ve
+
+#include <WiFi.h>
+#include <HTTPClient.h>
+#define SSID  "jaijujew"
+#define PASS  "yoki6969"
+#define SERVER_NAME "https://notify-api.line.me/api/notify"
+#define TOKEN "7ePoF1BYfYdNkzKMphtcB2Ncddi1IMEhZ0dX604zubi"
+
+
+
+BLYNK_WRITE(V0)
+{   
+  get1 = param.asInt();
+}
+
+
+
+
+Blynk.begin(BLYNK_AUTH_TOKEN,SSID,PASS);
+Blynk.run();
+Blynk.virtualWrite(V1, distance);
+*/
 
 void exam_1setup() {
   int myNumbers[] = {0, 2, 4, 16, 17};
@@ -122,3 +155,104 @@ void exam_3loop() {
   }
   delay(10);
 }
+/////////////////////////////////
+
+void exam_4setup() {
+  #define trig 21
+  #define echo 22
+  Servo servo;
+  Serial.begin(9600);
+  servo.attach(19);
+  pinMode(trig, OUTPUT);
+  pinMode(echo, INPUT);
+}
+void exam_4loop() {
+  long duration, distance;
+  digitalWrite(trig, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trig, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trig, LOW);
+  duration = pulseIn(echo, HIGH); 
+  distance = (duration * 0.0343) / 2;
+
+  int servoAngle = map(distance, 0, 30, 0, 180);
+  if (distance<=30) {
+  servo.write(servoAngle);
+  Serial.print("Distance: ");
+  Serial.print(distance);
+  Serial.print(" cm | Servo Angle: ");
+  Serial.println(servoAngle);
+  } else{
+    Serial.println("Out of range");
+  }
+  delay(100);
+}
+/////////////////////////////////////
+void exam_5setup() {
+  const int trig = 23;
+  const int echo = 22;
+  
+  LiquidCrystal lcd_1(0, 4, 5, 18, 16, 17);
+  
+  int bt = 35;
+  int poten = 34;
+  
+  bool a = true;
+  lcd_1.begin(16, 2);
+  Serial.begin(115200);
+
+  pinMode(trig, OUTPUT);
+  pinMode(echo, INPUT);
+  pinMode(bt, INPUT);
+  pinMode(poten, INPUT);
+}
+
+void exam_5loop() {
+  int check = digitalRead(bt);
+
+  if (check == 1) {
+    a = !a;
+    delay(1000);
+  }
+
+  int value = analogRead(poten);
+
+  digitalWrite(trig, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trig, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trig, LOW);
+
+  long duration = pulseIn(echo, HIGH);
+  int distance = duration * 0.034 / 2;
+
+  Serial.print("Distance: ");
+  Serial.println(distance);
+  Serial.println(a);
+
+  lcd_1.setCursor(0, 0);
+
+  if (a) {
+    lcd_1.print("Ultra_sonic!");
+    lcd_1.setCursor(0, 1);
+    lcd_1.print("Distance: ");
+    lcd_1.print(distance);
+    lcd_1.print(" CM ");
+  } else {
+    lcd_1.print("Potentio!     ");
+    lcd_1.setCursor(0, 1);
+    lcd_1.print("Value: ");
+    lcd_1.print(value);
+    lcd_1.print("                ");
+  }
+
+  delay(100);
+}
+//////////////////////////////////////
+/*void exam_6setup() {
+  
+}
+void exam_6loop() {
+  
+}*/
