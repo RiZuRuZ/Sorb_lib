2ceb7e210368501763a497ed615667e37f473891
RiZuRuZ <124576764+RiZuRuZ@users.noreply.github.com>
2024-02-19 21:06:28 +0700

    Update Sorb_lib.h

--- Sorb_lib.h
+++ Sorb_lib.h
@@ -1,7 +1,23 @@
-
 void exam_1setup();
 void exam_1loop();
 ////////////////
 
 void exam_2setup();
 void exam_2loop();
+////////////////
+
+void exam_3setup();
+void exam_3loop();
+
+////////////////
+
+void exam_4setup();
+void exam_4loop();
+////////////////
+
+void exam_5setup();
+void exam_5loop();
+////////////////
+
+//void exam_6setup();
+//void exam_6loop();
