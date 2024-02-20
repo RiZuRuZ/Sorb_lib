4e70295abc42cc47cd20be5c87df69bb30dd644f
RiZuRuZ <124576764+RiZuRuZ@users.noreply.github.com>
2024-02-20 15:26:28 +0700

    Update Sorb_lib.h

--- Sorb_lib.h
+++ Sorb_lib.h
@@ -18,21 +18,43 @@ void exam_4loop();
 void exam_5setup();
 void exam_5loop();
 ////////////////
-// LEDs on pins: 0, 2, 4, 16, 17
+// Example 1
+void exam_1setup();
+void exam_1loop();
+////////////////
+
+// Example 2
+void exam_2setup();
+void exam_2loop();
+////////////////
+
+// Example 3
+void exam_3setup();
+void exam_3loop();
+////////////////
+
+// Example 4
+void exam_4setup();
+void exam_4loop();
+////////////////
+
+// Example 5
+void exam_5setup();
+void exam_5loop();
+////////////////
+
+// LEDs on pins: 2, 4, 5, 12, 13
 // Example 1
 
-// LDR on pin 12, LEDs on pins: 2, 0, 4, 16
+// LDR on pin 13, LEDs on pins: 2, 4, 5, 12
 // Example 2
 
-// LEDs on pins: 0, 5, 18; Buzzer on pin 2
+// LEDs on pins: 4, 5, 18; Buzzer on pin 2
 // Example 3
 
 // Servo on pin 19, Ultrasonic sensor on pins 21 (Trig), 22 (Echo)
 // Example 4
 
-// Ultrasonic sensor on pins 23 (Trig), 22 (Echo); LCD on pins: 0, 4, 5, 18, 16, 17
+// Ultrasonic sensor on pins 23 (Trig), 22 (Echo); LCD on pins: 2, 4, 5, 12, 13, 14
 // Button on pin 35, Potentiometer on pin 34
 // Example 5
-
-//void exam_6setup();
-//void exam_6loop();
